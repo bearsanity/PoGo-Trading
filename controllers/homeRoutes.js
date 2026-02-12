ae60d566261cd34dff5ef9320a357cd7d6525ecc
bearsanity <jsweeney96@hotmail.com>
2026-02-11 22:24:18 -0330

    added home route

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const { Pokemon } = require('../models');
+
+router.get('/', async (req, res) =>{
+    try {
+        const pokemonData = await Pokemon.findAll({
+            order: [['id', 'ASC']],
+        });
+
+        const pokemon = pokemonData.map((pokemon) => pokemon.get({ plain: true }));
+
+        res.render('home', { pokemon });
+
+    } catch(err) {
+    res.status(500).json(err);
+ }
+});
+
